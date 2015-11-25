f79ffe20e07241d5f24d394740848b85f59ad87a
Patrick Roza <roza@sixnetworks.net>
2015-11-25 11:18:28 +0100

    Add cluster support

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
 var cp = require('child_process'),
 fs = require('fs');
 var express = require('express'),
-app = express(),
-port = process.env.port || 4000;
+cluster = require('express-cluster')
+port = process.env.port || 4000,
+outPath = "renders/";
 
-
-var outPath = "renders/";
-
-app.use((req, res, next) => {
-  var url = req.originalUrl; // not compatible when mounting on sub folder (or at least then need to keep that into account)
-  if (!url.startsWith("/http")) {
-    res.sendStatus(404);
-    return;
-  }
-  var outFile = outPath + 'out.html';
-  var outFileDetails = outFile + '.details.json';
-  var theUrl = url.substring(1);
-  if (theUrl.includes("_escaped_fragment_=")) {
-    res.status(400).send("Renderer should not get escaped fragment");
-    return;
-  }
-  var r = cp.spawnSync('node_modules\\.bin\\electron.cmd', ['main\\main.js', theUrl, outFile]);
-  console.log("result: ", r.status, r.stdout, r.stderr);
-  var document = fs.readFileSync(outFile, {encoding: 'utf-8'});
-  fs.unlink(outFile);
-  if (fs.existsSync(outFileDetails)) {
-    var details = JSON.parse(fs.readFileSync(outFileDetails, {encoding: 'utf-8'}));
-    fs.unlink(outFileDetails);
-    console.log("details: ", details);
-    // TODO: use details ??
-    if (details.httpResponseCode != 200) {
-      res.status(details.httpResponseCode).send(document);
+cluster(function(worker) {
+  var app = express();
+  app.use((req, res, next) => {
+    var url = req.originalUrl; // not compatible when mounting on sub folder (or at least then need to keep that into account)
+    if (!url.startsWith("/http")) {
+      res.sendStatus(404);
       return;
     }
-  }
-  res.send(document);
-});
+    var outFile = outPath + 'out-' + worker.id + '-' + Math.floor((Math.random() * 9999999) + 1) + '.html';
+    var outFileDetails = outFile + '.details.json';
+    var theUrl = url.substring(1);
+    if (theUrl.includes("_escaped_fragment_=")) {
+      res.status(400).send("Renderer should not get escaped fragment");
+      return;
+    }
+    var r = cp.spawnSync('node_modules\\.bin\\electron.cmd', ['main\\main.js', theUrl, outFile]);
+    console.log("result: ", r.status, r.stdout, r.stderr);
+    var document = fs.readFileSync(outFile, {encoding: 'utf-8'});
+    fs.unlink(outFile);
+    if (fs.existsSync(outFileDetails)) {
+      var details = JSON.parse(fs.readFileSync(outFileDetails, {encoding: 'utf-8'}));
+      fs.unlink(outFileDetails);
+      console.log("details: ", details);
+      // TODO: use details ??
+      if (details.httpResponseCode != 200) {
+        res.status(details.httpResponseCode).send(document);
+        return;
+      }
+    }
+    res.send(document);
+  });
 
-app.listen(port);
+  app.listen(port);
+}, {count: 2});
