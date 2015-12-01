06df80a06a32bfbaa65b4d004781eb65c5f16284
Patrick Roza <roza@sixnetworks.net>
2015-12-01 10:31:47 +0100

    fix meta/doc

--- public/api.ts
+++ public/api.ts
@@ -1,22 +1,26 @@
+"use strict";
+
 let ipc = require('ipc');
 
 let g = <any>global;
-function stripScripts(doc: HTMLDivElement) {
+function stripScripts(doc: Document) {
  let scripts = doc.getElementsByTagName('script');
  let i = scripts.length;
  while (i--) scripts[i].parentNode.removeChild(scripts[i]);
- return doc.innerHTML;
+ return doc.documentElement.outerHTML;
 }
 
-function getHeaders(doc: HTMLDivElement) {
+function getHeaders(doc: Document) {
   let head = doc.getElementsByTagName('head')[0];
   let meta = head.getElementsByTagName('meta');
   let headers: string[] = [];
   let status = "200";
   for (let i in meta) {
-    var m = meta[i];
-    if (m.getAttribute("name") == "prerender-status-code") status = m.getAttribute("content");
-    else if (m.getAttribute("name") == "prerender-header") headers.push(m.getAttribute("content"));
+    if (meta.hasOwnProperty(i)) {
+      var m = meta[i];
+      if (m.getAttribute("name") == "prerender-status-code") status = m.getAttribute("content");
+      else if (m.getAttribute("name") == "prerender-header") headers.push(m.getAttribute("content"));
+    }
   }
 
   let details: {httpResponseCode?: string, headers?: string[]} = {}
@@ -27,8 +31,10 @@ function getHeaders(doc: HTMLDivElement) {
 
 g.api = {
   "dumpHtml": (html: string) => {
-    let div = document.createElement('div');
-    div.innerHTML = html;
-    ipc.send('prerender:document', stripScripts(div), getHeaders(div));
+    var doc = document.implementation.createHTMLDocument('');
+    doc.open();
+    doc.write(html);
+    doc.close();
+    ipc.send('prerender:document', stripScripts(doc), getHeaders(doc));
   }
 }
